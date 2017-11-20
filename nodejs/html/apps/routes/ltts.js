02a443f71eeb82b8c5ef4c001fcbe41f05d09417
sinoory <sinoory@126.com>
2017-11-20 03:49:18 +0000

    add mltts page for mobile 2.0

--- nodejs/html/apps/routes/ltts.js
+++ nodejs/html/apps/routes/ltts.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('ltts/ltts', { title: 'Express' });
+  if(req.headers['user-agent'].match(/(iPhone|iPod|Android|ios)/i)){
+    res.render('ltts/mltts', { title: 'Express' });
+  }else{
+    res.render('ltts/ltts', { title: 'Express' });
+  }
 });
 
 module.exports = router;
